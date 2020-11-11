1564755fd42846bf3fcca99d53b0184da1d00327
Aliaksei Veras <aliaksei.veras@gmail.com>
2020-11-11 17:52:35 +0300

    TypeMetod

---
+++ TypeMetod/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TypeMetod
+{
+    class Program
+    {
+        public readonly Type Int = typeof(int);
+
+        static object argGetType(object arg)
+        {
+            string StringArg = (string) arg;
+            object Bufer;
+
+            try
+            {
+                Bufer = Convert.ToDouble(StringArg);
+                try
+                {
+                    Bufer = Convert.ToSByte(StringArg);
+                }
+                catch
+                {
+                    try
+                    {
+                        Bufer = Convert.ToUInt16(StringArg);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            Bufer = Convert.ToInt16(StringArg);
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                Bufer = Convert.ToUInt32(StringArg);
+                            }
+                            catch
+                            {
+                                try
+                                {
+                                    Bufer = Convert.ToInt32(StringArg);
+                                }
+                                catch
+                                {
+                                    try
+                                    {
+                                        Bufer = Convert.ToUInt64(StringArg);
+                                    }
+                                    catch
+                                    {
+                                        try
+                                        {
+                                            Bufer = Convert.ToInt64(StringArg);
+                                        }
+                                        catch
+                                        {
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                try
+                {
+                    Bufer = Convert.ToBoolean(StringArg);
+                }
+                catch
+                {
+                    try
+                    {
+                        Bufer = Convert.ToChar(StringArg);
+                    }
+                    catch
+                    {
+                        Bufer = StringArg;
+                    }
+                }
+            }
+
+            return $"{arg} is " + Bufer.GetType();
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(argGetType(Console.ReadLine())); 
+        }
+    }
+}
