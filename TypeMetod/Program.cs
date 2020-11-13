49b3102f198be2f6330badd428f95aa060a962aa
Aliaksei Veras <aliaksei.veras@gmail.com>
2020-11-13 10:30:19 +0300

    add switch

--- TypeMetod/Program.cs
+++ TypeMetod/Program.cs
@@ -8,7 +8,7 @@ namespace TypeMetod
     {
         public readonly Type Int = typeof(int);
 
-        static object argGetType(object arg)
+        static object argGetType(string arg)
         {
             string StringArg = (string) arg;
             object Bufer;
@@ -93,7 +93,23 @@ namespace TypeMetod
                 }
             }
 
-            return $"{arg} is " + Bufer.GetType();
+            switch (Bufer.GetType().Name)
+            {
+                case "String": return $"{arg} is string";
+                case "Byte": return $"{arg} is byte";
+                case "SByte": return $"{arg} is sbyte";
+                case "Int16": return $"{arg} is short";
+                case "UInt16": return $"{arg} is ushort";
+                case "Int32": return $"{arg} is int";
+                case "UInt32": return $"{arg} is uint";
+                case "Int64": return $"{arg} is long";
+                case "UInt64": return $"{arg} is ulong";
+                case "Char": return $"{arg} is char";
+                case "Boolen": return $"{arg} is bool";
+                case "Double": return $"{arg} is double";
+                case "Single": return $"{arg} is float";
+            }
+            return $"{arg} is хз";
         }
         
         static void Main(string[] args)
